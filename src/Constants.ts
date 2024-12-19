fba20e5fa58d3fccd515f3b73a4d72f59a07b1c1
Daniel Quah <danielquah148@yahoo.com>
2024-12-19 11:52:36 +1100

    Update API endpoint

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 
 
 // This should be in environment variable but it works for now
-export const FOBOH_BACKEND_URL = "http://localhost:3000"
+export const FOBOH_BACKEND_URL = "http://localhost:3000/api/v1"
